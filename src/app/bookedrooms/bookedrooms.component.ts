e450d235374f79e81f3b42a0cafbeb6f57372140
Nodarkashiashvili <n.qashiashvili@icloud.com>
2025-08-25 08:25:19 -0700

    bolo today

---
+++ src/app/bookedrooms/bookedrooms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookedrooms',
+  imports: [],
+  templateUrl: './bookedrooms.component.html',
+  styleUrl: './bookedrooms.component.scss'
+})
+export class BookedroomsComponent {
+
+}
