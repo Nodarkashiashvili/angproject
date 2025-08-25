071c44ff126b1a788bc87ec6f3d7f3523b365bc0
Nodarkashiashvili <n.qashiashvili@icloud.com>
2025-08-25 07:42:39 -0700

    nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  showmobilemenu = false
+showhidemenu(){
+  this.showmobilemenu == false ? this.showmobilemenu = true : this.showmobilemenu = false
+}
 }
