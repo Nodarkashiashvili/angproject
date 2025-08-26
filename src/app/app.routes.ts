6e1951a0e8e6c661a6d44f8a2bd2b8f24d7fecfb
Nodarkashiashvili <n.qashiashvili@icloud.com>
2025-08-26 06:53:32 -0700

    routermodule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { BookedroomsComponent } from './bookedrooms/bookedrooms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {path: 'home', 
+        component: HomeComponent
+
+    },
+    {
+        path: 'rooms',
+        component : RoomsComponent
+    },
+    {
+        path: "hotels",
+        component: HotelsComponent
+    },
+    {path: 'bookedrooms',
+        component: BookedroomsComponent
+
+    },
+
+
+
+
+
+];
