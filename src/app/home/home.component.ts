504c2422cb9ae6903ac828ccc523cde6b94cf74d
Nodarkashiashvili <n.qashiashvili@icloud.com>
2025-09-01 09:07:14 -0700

    callll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonfunctionService } from '../services/commonfunction.service';
+import { ApihttpService } from '../services/apihttp.service';
 
 @Component({
   selector: 'app-home',
   imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
-
-  
+  constructor(
+    private commonfun: CommonfunctionService,
+    private http: ApihttpService,
+  ) {}
 }
