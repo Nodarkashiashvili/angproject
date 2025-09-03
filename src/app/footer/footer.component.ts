23c5f188aeeccc46daa718558b5457c10ac18612
Nodarkashiashvili <n.qashiashvili@icloud.com>
2025-09-02 18:09:24 -0700

    date

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [HomeComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+cunrentDate = new Date()
 
 }
