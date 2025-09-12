063f1657186b8db14b199243a2e1c5b09ff2d2a9
Nodarkashiashvili <n.qashiashvili@icloud.com>
2025-09-12 11:36:26 -0700

    scs

--- src/app/bookedrooms/bookedrooms.component.ts
+++ src/app/bookedrooms/bookedrooms.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ApihttpService } from '../services/apihttp.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bookedrooms',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bookedrooms.component.html',
   styleUrl: './bookedrooms.component.scss'
 })
 export class BookedroomsComponent {
+constructor (private http : ApihttpService){}
+
+ngOnInit(){
+  this.http.getData('https://hotelbooking.stepprojects.ge/api/Booking')
+  .subscribe((resp : any)=>{
+    console.log(resp)
+    this.booked=resp.slice(resp.length-7,resp.length-1)
+    console.log(this.booked)
+  })
+}
+booked : any [] = []
 
 }
